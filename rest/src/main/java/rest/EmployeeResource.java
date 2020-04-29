083c3f01fd787d96c45b26cd2b5d6893b2fa423b
jonasb <jonaasbalaazs@gmail.com>
2020-04-29 15:56:22 +0200

    jax-rs example 2

--- rest/src/main/java/rest/EmployeeResource.java
+++ rest/src/main/java/rest/EmployeeResource.java
@@ -1,21 +1,28 @@
 package rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/api/employee")
+@Path("api/employee")
 public class EmployeeResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.TEXT_XML)
     public List<Employee> getEmployees() {
         var does = List.of("John Doe", "Jane Doe", "Jack Doe");
         return does.stream().map(Employee::new).collect(Collectors.toList());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Employee createEmployee(Employee employee)  {
+        System.out.println("create "+employee);
+        return new Employee(employee.getName().toUpperCase());
     }
 
 
